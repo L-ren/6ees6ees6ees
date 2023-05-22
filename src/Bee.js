7da5a8862febcd5958ac1fc28b4ed31fcc2ea72f
Loren <loren.johnson99@yahoo.com>
2023-05-22 12:03:12 -0700

    assignment complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  // call super method in subclass constructor if using this keyword or constructor method
+  constructor() {
+    super(); // instantiates parent class
+    this.age  = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing'
+  }
 };
